fb0017f10ad0db3ffdc6c22c6bf9928bf721ba65
Till <till@e-bot7.com>
2019-04-22 12:44:36 +0200

    Make vectorize.py deterministic

--- utils/vectorize.py
+++ utils/vectorize.py
@@ -1,8 +1,12 @@
 import numpy as np
+import hashlib
 
 
 def vectorize(profile):
     assert '_id' in profile
     assert 'name' in profile
     assert 'surname' in profile
-    return np.random.rand(1024)
+    profile_summary = '_'.join(profile.values()).encode('utf-8')
+    hash_object = hashlib.sha512(profile_summary)
+    hex_dig = hash_object.hexdigest()
+    return np.asarray([int(i, 16)/64 for i in hex_dig])
