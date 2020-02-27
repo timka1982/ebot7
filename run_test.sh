7f108ff1f4a9bdb65f359e56bce5ca8f57d1c307
Till Bauer <till@e-bot7.com>
2020-02-27 17:48:26 +0100

    Fix path

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 image_id=$(docker build -q . | awk -F':' '{print $2}')
-docker run $image_id python test_suite.py
+docker run $image_id python python-coding-challenge-test/test_suite.py
