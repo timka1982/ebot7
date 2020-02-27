c3074ac9eb7203998d5fd491368c8d7512f077ec
Till Bauer <till@e-bot7.com>
2020-02-27 17:43:28 +0100

    Change to use new test_suite logic

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 image_id=$(docker build -q . | awk -F':' '{print $2}')
-docker run $image_id python -m unittest discover -s python-coding-challenge-test -p "*test*.py"
+docker run $image_id python test_suite.py
