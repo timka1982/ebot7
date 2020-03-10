4145b55c4afb1b1642ab72d3e33d476b0a7e8502
Sumit Dugar <sumit@e-bot7.com>
2020-03-10 11:21:14 +0100

    ML-776 fix test-suite path

--- run_test.sh
+++ run_test.sh
@@ -2,4 +2,4 @@
 
 git clone git@github.com:ebot7/python-coding-challenge-test.git
 image_id=$(docker build -q . | awk -F':' '{print $2}')
-docker run $image_id cd python-coding-challenge-test && python test_suite.py
+docker run $image_id /bin/sh -c 'cd python-coding-challenge-test && python test_suite.py'
